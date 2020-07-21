638585653571991dd6c849fbf5fb806af43b7ebc
liupeng-sunny <1810700779@qq.com>
2020-07-21 12:11:53 +0800

    uploading

---
+++ Anonymous.java
@@ -0,0 +1,11 @@
+package cn.liu.study.Test;
+
+public class Anonymous {
+
+    public static void main(String[] args) {
+        Person one = new Person();
+        one.name = "杰克逊";
+        one.showName();
+    }
+
+}
---
+++ Person.java
@@ -0,0 +1,11 @@
+package cn.liu.study.Test;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public void  showName(){
+        System.out.println("我叫" + name);
+    }
+}
